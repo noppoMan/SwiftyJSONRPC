055feba6643f5a22f39971ed325e38d991988ee3
noppoman <yuki@miketokyo.com>
2016-12-05 19:31:53 +0900

    add Xcode proj

---
+++ Xcode/SwiftyJSONRPC/SwiftyJSONRPC.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyJSONRPC.h
+//  SwiftyJSONRPC
+//
+//  Created by Yuki Takei on 2016/12/05.
+//  Copyright © 2016 Miketokyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyJSONRPC.
+FOUNDATION_EXPORT double SwiftyJSONRPCVersionNumber;
+
+//! Project version string for SwiftyJSONRPC.
+FOUNDATION_EXPORT const unsigned char SwiftyJSONRPCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyJSONRPC/PublicHeader.h>
+
+
